bf02c7a4d7df21ba114451e7cf23c27339a5bcf0
Lhtlakshan <thilinalakshanhewage1999@gmail.com>
2025-12-09 20:39:48 +0530

    feature: product card

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,23 @@ import Login from './form/Login'
 import Signup from './form/Register'
 import { Toaster } from 'react-hot-toast'
 import Dashboard from './pages/Dashboard'
+import Products from './pages/Products'
 
 function App() {
 
   return (
     <>
-    <Toaster
-  position="top-right"
-  reverseOrder={false}
-/>
+      <Toaster position="top-right" reverseOrder={false} />
       <BrowserRouter>
         <Routes>
-          <Route path = "/" element={<Dashboard/>}/>
-          <Route path = "/login" element={<Login/>}/>
-          <Route path = "/Register" element={<Signup/>}/>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Signup />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/pages/Products.tsx
@@ -0,0 +1,16 @@
+import ProductCard from "../components/ProductCard";
+import paddy from "../assets/paddy.jpg";
+
+const Products = () => {
+  return (
+    <ProductCard
+      image={paddy}
+      name="Organic Tomatoes"
+      price={450}
+      description="Fresh organic tomatoes directly from local farmers."
+      onAddToCart={() => console.log("Added to cart!")}
+    />
+  );
+}
+
+export default Products
